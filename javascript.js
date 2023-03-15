ce14b8cc8f64194aad0049f44cbe1eceaf695388
Ankit Sharma <ankitsharma2801@gmail.com>
2023-03-15 12:28:44 +0530

    Edit toLowerCase syntax, define playRound outside.
    - Corrected toLowerCase syntax
    - Defined playRound outside of game()
    - Made pwin and cwin global variables
    - Other small non significant changes

--- javascript.js
+++ javascript.js
@@ -2,65 +2,76 @@ function getComputerChoice() {
     //getting random no from 0 to 2
     rnum = Math.floor(Math.random() * 3);
     //converting random no into random choice
-    if(rnum === 0)
+    if (rnum === 0)
         return "rock";
-    else if( rnum === 1)
+    else if (rnum === 1)
         return "paper";
     else
         return "scissors";
 }
 
+//following integers display no of game won by each party
+let pwin = 0;
+let cwin = 0;
+
 //call this function to start the game
 function game() {
     console.log("Welcome to the game");
-    //following integers display no of game won by each party
-    pwin = 0;
-    cwin = 0;
     //loop to play 5 games
-    for(i = 1; i <= 5; i++) {
+    for (i = 1; i <= 5; i++) {
         //display game number
         console.log(`Game ${i}`);
         console.log("Your have to select between rock/paper/scissors");
-        playerSelection = toLowerCase(prompt("Enter selection to begin game: "));
+        playerSelection = (prompt("Enter selection to begin game: ")).toLowerCase();
         computerSelection = getComputerChoice();
-        //round starts
-        function playRound(playerSelection, computerSelection) {
-            if(playerSelection === computerSelection)
-                whoWinRound("draw");
-            if(playerSelection === "rock" && computerSelection == "scissors")
-                whoWinRound("player");
-                pwin ++;
-            if(playerSelection === "rock" && computerSelection == "paper")
-                whoWinRound("computer");
-                cwin ++;
-            if(playerSelection === "scissors" && computerSelection == "rock")
-                whoWinRound("computer");
-                cwin ++;
-            if(playerSelection === "scissors" && computerSelection == "paper")
-                whoWinRound("player");
-                pwin ++;
-            if(playerSelection === "paper" && computerSelection == "rock")
-                whoWinRound("player");
-                pwin ++;
-            if(playerSelection === "paper" && computerSelection == "scissors")
-                whoWinRound("computer"); 
-                cwin ++;
-        }
+        playRound(playerSelection, computerSelection);
     }
-    //condition to decide who wins the game
-    if(pwin > cwin)
-        console.log("player wins the game");
-    else 
-        console.log("computer wins the game");
 
+    //condition to decide who wins the game
+    if (pwin === cwin)
+        console.log("This game is a draw");
+    else if (pwin > cwin)
+        console.log("Player wins the game");
+    else
+        console.log("Computer wins the game");
+}
+    
+//Call this function to play round
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === computerSelection)
+        whoWinRound("draw");
+    else if (playerSelection === "rock" && computerSelection == "scissors") {
+        whoWinRound("player");
+        return pwin++;
+    }
+    else if (playerSelection === "rock" && computerSelection == "paper") {
+        whoWinRound("computer");
+        return cwin++;
+    }
+    else if (playerSelection === "scissors" && computerSelection == "rock") {
+        whoWinRound("computer");
+        return cwin++;
+    }
+    else if (playerSelection === "scissors" && computerSelection == "paper") {
+        whoWinRound("player");
+        return pwin++;
+    }
+    else if (playerSelection === "paper" && computerSelection == "rock") {
+        whoWinRound("player");
+        return pwin++;
+    }
+    else if (playerSelection === "paper" && computerSelection == "scissors") {
+        whoWinRound("computer");
+        return cwin++;
+    }
 }
 
 //call this function to say who wins the round
 function whoWinRound(name) {
-    if(name === "player")
+    if (name === "player")
         console.log("Player wins this round");
-    else if(name === "computer")
+    else if (name === "computer")
         console.log("Computer wins this round");
-    else 
+    else
         console.log("This round is a draw")
 }
